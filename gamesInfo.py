ce50952c7ea8c1ad26dd7afc488844d69ea9cd66
jfisher993 <jfisher993@gmail.com>
2016-06-13 21:20:51 -0400

    Update gamesInfo.py

--- gamesInfo.py
+++ gamesInfo.py
@@ -12,6 +12,26 @@ from flask import make_response
 app = Flask(__name__)
 
 
+myGames = []
+
+class GameInfo:
+    def __init__(self, gameName, gameInfoVoice, gameInfo):
+        self.gameName = gameName
+        self.gameInfoVoice = gameInfoVoice
+        self.gameInfo = gameInfo
+
+myGames.append("Haberdashery - Endless Arcade Runner", "Available on iOS and Android!", "iOS: http://apple.co/1UrkmPR \nAndroid: http://bit.ly/1RqflCQ")
+myGames.append("Target Crack", "Available on iOS and Android!", "iOS: http://apple.co/1Lvgapx \nAndroid: http://bit.ly/1ttbDTt")
+myGames.append("Turbo Hobo Cannon", "Available on Android and Newgrounds!", "Android: http://bit.ly/1S35i53 \nNewgrounds: http://bit.ly/1VW2vSh")
+myGames.append("Fungeon Crawler", "Available on Indie Game Stand for PC and MAC!", "PC/MAC: http://bit.ly/1Yn7cVr")
+myGames.append("A World Apart", "Available on Indie Game Stand for PC and MAC!", "PC/MAC: http://bit.ly/1MhMy02")
+myGames.append("Zap it", "Available on iOS and Android!", "iOS: http://apple.co/1VW28qB \nAndroid: http://bit.ly/1UPvi5G")
+myGames.append("Switch it", "Available on iOS and Android!", "iOS: http://apple.co/1UPvt0G \nAndroid: http://bit.ly/1UMB1fg")
+myGames.append("Defuse it", "Available on iOS and Android!", "iOS: http://apple.co/1D1kOtD \nAndroid: http://bit.ly/1Ua6z1N")
+myGames.append("Don't Touch the Button", "Available on iOS!", "iOS: http://apple.co/1ttcNhH")
+myGames.append("Welcome to Undercog", "by Geek Monster Games!", "by Geek monster Games! \nLearn more at: http://bit.ly/1VW2HAL")
+myGames.append("Rocket Rambler", "by Heavy Key Studios!", "by Heavy Key Studios! \niOS: http://apple.co/1UPw5DG \nAndroid: http://bit.ly/1YoohP8")
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
@@ -37,41 +57,11 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     gameName = parameters.get("gameName")
 
-    if (gameName == "Haberdashery"):
-        gameInfoVoice = "Haberdashery - Endless Arcade Runner! Available on iOS and Android!"
-        gameInfo = "Haberdashery - Endless Arcade Runner! \niOS: http://apple.co/1UrkmPR \nAndroid: http://bit.ly/1RqflCQ"
-    elif (gameName == "Target Crack"):
-        gameInfoVoice = "Target Crack! Available on iOS and Android!"
-        gameInfo = "Target Crack! \niOS: http://apple.co/1Lvgapx \nAndroid: http://bit.ly/1ttbDTt"
-    elif (gameName == "Turbo Hobo Cannon"):
-        gameInfoVoice = "Turbo Hobo Cannon! Available on Android and Newgrounds!"
-        gameInfo = "Turbo Hobo Cannon! \nAndroid: http://bit.ly/1S35i53 \nNewgrounds: http://bit.ly/1VW2vSh"
-    elif (gameName == "Fungeon Crawler"):
-        gameInfoVoice = "Fungeon Crawler! Available on Indie Game Stand for PC and MAC!"
-        gameInfo = "Fungeon Crawler \nPC/MAC: http://bit.ly/1Yn7cVr"
-    elif (gameName == "A World Apart"):
-        gameInfoVoice = "A World Apart! Available on Indie Game Stand for PC and MAC!"
-        gameInfo = "A World Apart \nPC/MAC: http://bit.ly/1MhMy02"
-    elif (gameName == "Zap it"):
-        gameInfoVoice = "Zap it! Available on iOS and Android!"
-        gameInfo = "Zap it \niOS: http://apple.co/1VW28qB \nAndroid: http://bit.ly/1UPvi5G"
-    elif (gameName == "Switch it"):
-        gameInfoVoice = "Switch it! Available on iOS and Android!"
-        gameInfo = "Switch it \niOS: http://apple.co/1UPvt0G \nAndroid: http://bit.ly/1UMB1fg"
-    elif (gameName == "Defuse it"):
-        gameInfoVoice = "Defuse it! Available on iOS and Android!"
-        gameInfo = "Defuse it \niOS: http://apple.co/1D1kOtD \nAndroid: http://bit.ly/1Ua6z1N"
-    elif (gameName == "Don't Touch the Button"):
-        gameInfoVoice = "Don't Touch the Button! Available on iOS!"
-        gameInfo = "Don't Touch the Button \niOS: http://apple.co/1ttcNhH"
-    elif (gameName == "Welcome to Undercog"):
-        gameInfoVoice = "Welcome to Undercog!"
-        gameInfo = "Geek Monster Games - Welcome to Undercog \nLearn more at: http://bit.ly/1VW2HAL"
-    elif (gameName == "Rocket Rambler"):
-        gameInfoVoice = "Rocket Rambler! Available on iOS and Android!"
-        gameInfo = "Heavy Key Studios - Rocket Rambler \niOS: http://apple.co/1UPw5DG \nAndroid: http://bit.ly/1YoohP8" 
-    else:
-        gameInfo = "Game not found"
+    for game in myGames:
+        if (game.gameName == gameName):
+            gameInfoVoice = game.gameName + "! " + game.gameInfoVoice
+            gameInfo = game.gameName + "!\n" + game.gameInfo
+
 
     return {
         "speech": gameInfo,
