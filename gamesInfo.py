74538e146a5a35d37979e760457dafcde805174f
jfisher993 <jfisher993@gmail.com>
2016-06-13 20:25:48 -0400

    Update gamesInfo.py

--- gamesInfo.py
+++ gamesInfo.py
@@ -39,37 +39,37 @@ def makeWebhookResult(req):
 
     if (gameName == "Haberdashery"):
         gameInfoVoice = "Haberdashery - Endless Arcade Runner! Available on iOS and Android!"
-        gameInfo = "Haberdashery - Endless Arcade Runner! \n iOS: http://apple.co/1UrkmPR \n Android: http://bit.ly/1RqflCQ"
+        gameInfo = "Haberdashery - Endless Arcade Runner! \niOS: http://apple.co/1UrkmPR \nAndroid: http://bit.ly/1RqflCQ"
     elif (gameName == "Target Crack"):
         gameInfoVoice = "Target Crack! Available on iOS and Android!"
-        gameInfo = "Target Crack \n iOS: http://apple.co/1Lvgapx \n Android: http://bit.ly/1ttbDTt"
+        gameInfo = "Target Crack! \niOS: http://apple.co/1Lvgapx \nAndroid: http://bit.ly/1ttbDTt"
     elif (gameName == "Turbo Hobo Cannon"):
         gameInfoVoice = "Turbo Hobo Cannon! Available on Android and Newgrounds!"
-        gameInfo = "Target Crack \n Android: http://bit.ly/1S35i53 \n Newgrounds: http://bit.ly/1VW2vSh"
+        gameInfo = "Turbo Hobo Cannon! \nAndroid: http://bit.ly/1S35i53 \nNewgrounds: http://bit.ly/1VW2vSh"
     elif (gameName == "Fungeon Crawler"):
         gameInfoVoice = "Fungeon Crawler! Available on Indie Game Stand for PC and MAC!"
-        gameInfo = "Fungeon Crawler \n PC/MAC: http://bit.ly/1Yn7cVr"
+        gameInfo = "Fungeon Crawler \nPC/MAC: http://bit.ly/1Yn7cVr"
     elif (gameName == "A World Apart"):
         gameInfoVoice = "A World Apart! Available on Indie Game Stand for PC and MAC!"
-        gameInfo = "A World Apart \n PC/MAC: http://bit.ly/1MhMy02"
+        gameInfo = "A World Apart \nPC/MAC: http://bit.ly/1MhMy02"
     elif (gameName == "Zap it"):
         gameInfoVoice = "Zap it! Available on iOS and Android!"
-        gameInfo = "Zap it \n iOS: http://apple.co/1VW28qB \n Android: http://bit.ly/1UPvi5G"
+        gameInfo = "Zap it \niOS: http://apple.co/1VW28qB \nAndroid: http://bit.ly/1UPvi5G"
     elif (gameName == "Switch it"):
         gameInfoVoice = "Switch it! Available on iOS and Android!"
-        gameInfo = "Switch it \n iOS: http://apple.co/1UPvt0G \n Android: http://bit.ly/1UMB1fg"
+        gameInfo = "Switch it \niOS: http://apple.co/1UPvt0G \nAndroid: http://bit.ly/1UMB1fg"
     elif (gameName == "Defuse it"):
         gameInfoVoice = "Defuse it! Available on iOS and Android!"
-        gameInfo = "Defuse it \n iOS: http://apple.co/1D1kOtD \n Android: http://bit.ly/1Ua6z1N"
+        gameInfo = "Defuse it \niOS: http://apple.co/1D1kOtD \nAndroid: http://bit.ly/1Ua6z1N"
     elif (gameName == "Don't Touch the Button"):
         gameInfoVoice = "Don't Touch the Button! Available on iOS!"
-        gameInfo = "Don't Touch the Button \n iOS: http://apple.co/1ttcNhH"
+        gameInfo = "Don't Touch the Button \niOS: http://apple.co/1ttcNhH"
     elif (gameName == "Welcome to Undercog"):
         gameInfoVoice = "Welcome to Undercog!"
-        gameInfo = "Geek Monster Games - Welcome to Undercog \n Learn more at: http://bit.ly/1VW2HAL"
+        gameInfo = "Geek Monster Games - Welcome to Undercog \nLearn more at: http://bit.ly/1VW2HAL"
     elif (gameName == "Rocket Rambler"):
         gameInfoVoice = "Rocket Rambler! Available on iOS and Android!"
-        gameInfo = "Heavy Key Studios - Rocket Rambler \n iOS: http://apple.co/1UPw5DG \n Android: http://bit.ly/1YoohP8" 
+        gameInfo = "Heavy Key Studios - Rocket Rambler \niOS: http://apple.co/1UPw5DG \nAndroid: http://bit.ly/1YoohP8" 
     else:
         gameInfo = "Game not found"
 
